ca65839e64ea9580cbab2b64e88942bfffd54700
lmkiernan <liamkiernan03@gmail.com>
2025-06-10 12:02:33 -0400

    added API Key

--- src/embedding.py
+++ src/embedding.py
@@ -1,5 +1,11 @@
 
 def embed(chunk, config):
         for model in config["embedding"]:
-            print(model["provider"])
-            print(chunk["chunk_id"])
+            if model["provider"] == "openai":
+                 embed_openai(chunk, model)
+
+def embed_openai(chunk, config):
+    print(chunk["chunk_id"])
+
+def embed_huggingface(chunk, config):
+    print(chunk["chunk_id"])
