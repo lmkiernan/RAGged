a6e9ab1f91ce863e48570a19e01fba3a5939ae74
lmkiernan <liamkiernan03@gmail.com>
2025-06-11 23:10:36 -0400

    search feature added to vectoreStore

--- src/embedding_router.py
+++ src/embedding_router.py
@@ -2,7 +2,7 @@ import json
 import os
 from src.Embedding import OpenAIEmbedder, HFEmbedder
 import time
-
+from src.vectorStore import upsert_vector
 # Adjust the path as needed
 with open(os.path.join(os.path.dirname(__file__), '..', 'APIKeys.json')) as f:
        api_keys = json.load(f)
@@ -25,8 +25,16 @@ def embed_openai(chunk, config):
         vector = embedder.embed(chunk["text"])
         t1 = time.time()
         latency = (t1 - t0) *1000
-        chunk[""]
-        print(f"Latency= {latency}")
+        payload = {
+            "chunk_id": chunk["chunk_id"],
+            "source": chunk["source"],
+            "strategy": chunk["strategy"],
+            "token_count": list(chunk["tokens"].values())[0],
+            "latency": latency,
+            "cost":  list(chunk["tokens"].values())[0] * model["pricing_per_1k_tokens"] / 1000
+
+        }
+        upsert_vector(vector, payload, chunk["chunk_id"])
     
 def embed_huggingface(chunk, config):
      for model in config["huggingface"]:
--- src/vectorStore.py
+++ src/vectorStore.py
@@ -1,1 +1,31 @@
 import qdrant_client
+from qdrant_client import QdrantClient
+import uuid
+
+client = QdrantClient(url="http://localhost:6333")
+
+
+def uuid_from_string(s: str) -> str:
+    # Use a namespace (here, DNS is common, but you can use your own)
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, s))
+
+# Example payload
+def upsert_vector(vector: list[float], payload: dict, id: str):
+    client.upsert(
+        collection_name="autoembed_chunks",
+        points=[{
+            "id": uuid_from_string(id),
+            "vector": vector,
+            "payload": payload
+        }]
+    )
+
+def search(query_vector: list[float], top_k: int = 5, filter: dict = None):
+
+    return client.search(
+        collection_name="autoembed_chunks",
+        query_vector=query_vector,
+        limit=top_k,
+        query_filter=filter
+    )
+
