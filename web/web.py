a307dee70c683f088ef9cf727d72276b8f800be1
lmkiernan <liamkiernan03@gmail.com>
2025-06-17 11:52:47 -0400

    fixed async call of upload_json

--- web/web.py
+++ web/web.py
@@ -197,7 +197,7 @@ async def process_documents():
                 fname = file['name']
                 text = supabase_client.get_json_field(fname, user_id, "processed/", "text")
                 texts[fname] = text
-            supabase_client.upload_json(texts, "qa_pairs.json", user_id, "qa_pairs")
+            await supabase_client.upload_json(texts, "qa_pairs.json", user_id, "qa_pairs")
             
             # Verify QA pairs were created
             try:
