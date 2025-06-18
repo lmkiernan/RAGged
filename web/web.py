eb2c44d49e9e1d8477b190595a7b31217c4b47d4
lmkiernan <liamkiernan03@gmail.com>
2025-06-18 16:49:13 -0400

    attempt to just use en

--- web/web.py
+++ web/web.py
@@ -7,6 +7,8 @@ import sys
 import asyncio
 import uuid
 import supabase
+import spacy
+
 
 # Add the parent directory to Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
